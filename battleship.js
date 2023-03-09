3497ed06f47b8ef9d78c06156d5b2e6e65b81c2f
Koshkaflo <glebovanna94@gmail.com>
2023-03-09 19:39:20 +0300

    changed prop ships

--- battleship.js
+++ battleship.js
@@ -75,9 +75,9 @@ let model = {
     shipSunk: 0,
 
     ships: [
-        {locations: ['06', '16', '26'], hits: ['', '', '']},
-        {locations: ['24', '34', '44'], hits: ['', '', '']},
-        {locations: ['10', '11', '12'], hits: ['', '', '']}],
+        {locations: [0, 0, 0], hits: ['', '', '']},
+        {locations: [0, 0, 0], hits: ['', '', '']},
+        {locations: [0, 0, 0], hits: ['', '', '']} ],
     
     fire: function(guess) {
         for (let i = 0; i < this.numShips; i++) {
@@ -115,6 +115,38 @@ let model = {
             } while (this.collision(locations));
             this.ships[i].locations = locations;
         }
+    },
+    generateShip: function() {
+        let direction = Math.floor(Math.random() * 2);
+        let row, col;
+
+        if (direction === 1) {
+            row = Math.floor(Math.random() * this.boardSize);
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+        }else {
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+        let newShipLocations = [];
+        for (let i = 0; i < this,this.shipLength; i++) {
+            if (direction === 1) {
+                newShipLocations.push(row + '' + (col + i));
+            } else {
+                newShipLocations.push((row + i) + '' + col);
+            }
+        }
+        return newShipLocations;
+    },
+    collision: function(locations) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = model.ships[i];
+            for (let j = 0; j < locations.length; j++) {
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
 
@@ -203,6 +235,8 @@ function init() {
     fireButton.onclick = handleFireButton;
     let guessInput = document.getElementById('guessInput');
     guessInput.onkeydown = handleKeyPress;
+
+    model.generateShipLocations();
 };
 
 function handleFireButton() {
